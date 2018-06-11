79805bbec3d0c7e43e20278d0281656154439575
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-11 12:36:56 -0700

    factor out new function for yearOfBirth

--- function-drills/function-drill-1.js
+++ function-drills/function-drill-1.js
@@ -1,9 +1,12 @@
 'use strict';
+function yearOfBirth(age) {
+  let yearOfBirth = 2018 - age;
+  console.log("I was born in " + yearOfBirth);
+}
 
 function whoAmI(name, age) {
-  let yearOfBirth = 2018 - age;
   console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
-  console.log("I was born in " + yearOfBirth);
+  yearOfBirth(age);
 }
 
 whoAmI('Kyle', 35);
