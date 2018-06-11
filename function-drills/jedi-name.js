0f9233072918de6db0bcaf64ae502d76c9d7bc95
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-11 15:54:59 -0700

    implement a working version of jedi-name.js using an array with for loops

--- function-drills/jedi-name.js
+++ function-drills/jedi-name.js
@@ -1,6 +1,17 @@
 'use strict';
 const jediName = (firstName, lastName) => {
+  // grab the first 3 letters of lastName and store in a var
+  let jediConstruct = [];
+  for (let i = 0; i < 3; i++) {
+    jediConstruct.push(lastName[i]);
+  }
 
+  // grab the first 2 letters of firstName and store in a var
+  for (let i = 0; i < 2; i++) {
+    jediConstruct.push(firstName[i]);
+  }
+  let jediName = jediConstruct.join('');
+  return jediName;
 };
 
 let test1 = jediName('Beyonce', 'Knowles');
