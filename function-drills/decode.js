1f40419d0e242ffa23101542b8bdf62db4f5badd
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-11 16:45:04 -0700

    add working implementation for decode.js

--- function-drills/decode.js
+++ function-drills/decode.js
@@ -1,7 +1,43 @@
 'use strict';
-const decode = () => {
+const decode = (word) => {
+  let decodedLetter;
+  switch (word[0]) {
+  case 'a' : {
+    decodedLetter = word[1];
+    break;
+  }
+  case 'b' : {
+    decodedLetter = word[2];
+    break;
+  }
+  case 'c' : {
+    decodedLetter = word[3];
+    break;
+  }
+  case 'd' : {
+    decodedLetter = word[4];
+    break;
+  }
+  default : {
+    decodedLetter = ' ';
+  }
+  // close switch statement
+  }
 
+  return decodedLetter;
 };
 
-let test1 = decode('craft block argon meter bells brown croon droop');
+const decodeMessage = (message) => {
+  let decodedConstructor = [];
+  let words = message.split(' ');
+  words.forEach(word => {
+    let newLetter = decode(word);
+    decodedConstructor.push(newLetter);
+  });
+
+  let decodedMessage = decodedConstructor.join('');
+  return decodedMessage;
+};
+
+let test1 = decodeMessage('craft block argon meter bells brown croon droop');
 console.log(test1);
