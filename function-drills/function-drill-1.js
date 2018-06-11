1400ad6fed287e9b872db6239df7e4cbb398238b
Kyle Bowen <kyle.m.bowen@gmail.com>
2018-06-11 14:56:46 -0700

    WIP birthday check implementation

--- function-drills/function-drill-1.js
+++ function-drills/function-drill-1.js
@@ -1,19 +1,34 @@
 'use strict';
 function yearOfBirth(age) {
-  let yearOfBirth = 2018 - age;
-  console.log("I was born in " + yearOfBirth); 
   try {
     if (age < 0) throw "Age can not be negative";
   }
   catch(err) { 
     console.log("Error: " + err + ".")
   }
-  return 2018-age;
+  // Need to know birthday && today's day
+  // Need to know if the birthday has already passed in current year
+  // if it's passed, current year - age
+  // if not passed yet, current year - age - 1
+  let yearOfBirth = 2018 - age;
+  const birthdayInFuture = (birthday) => {
+    // if in future return true
+    Date.now()
+    // if in past (or today) return false
+  }
+
+  if (birthdayInFuture(birthday)) {
+    yearOfBirth = currentYear - age - oneYear;
+  }
+  if (!birthdayInFuture(birthday)) {
+    yearOfBirth = currentYear - age;
+  }
+  return yearOfBirth;
 }
 
 function whoAmI(name, age) {
   try {
-    if (!(name && age)) {
+    if (!name || !age) {
       throw 'Arguments not valid';
     }
     if (typeof age !== 'number') {
@@ -24,7 +39,7 @@ function whoAmI(name, age) {
     }
 
     console.log(`Hi, my name is ${name} and I'm ${age} years old.`);
-    yearOfBirth(age);
+    console.log("I was born in " + yearOfBirth(age));
 
   }  
   
@@ -34,4 +49,9 @@ function whoAmI(name, age) {
 
 }
 
-whoAmI('Kyle', "35");
+// Kyle was born July 20, 1982, but since it's 
+// not yet July the yearOfBirth is off by one.
+// whoAmI('Kyle', 35) should return:
+//  "Hi, my name is Kyle and I'm 35 years old.
+//   I was born in 1982."
+whoAmI('Kyle', 35);
